4d8767ca54818c153366611538a6d9f1de953d4f
An average Beanmann <102012974+AnaverageBeanmann@users.noreply.github.com>
2022-03-25 21:07:45 -0400

    spawnable b0ne(r) varients

---
+++ lua/entities/npc_vj_bbohg_boner_frenzy/shared.lua
@@ -0,0 +1,8 @@
+ENT.Base 			= "npc_vj_bbohg_boner" -- List of all base types: https://github.com/DrVrej/VJ-Base/wiki/Base-Types
+ENT.Type 			= "ai"
+ENT.PrintName 		= "b0ne(r)"
+ENT.Author 			= "Bean"
+ENT.Contact 		= "http://steamcommunity.com/groups/vrejgaming"
+ENT.Purpose 		= "Spooky Scary Skeletons, send shivers down your spine."
+ENT.Instructions 	= "Click on the spawnicon to spawn it."
+ENT.Category		= "BBOHG"
